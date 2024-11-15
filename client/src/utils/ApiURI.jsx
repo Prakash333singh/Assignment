58896cfabacd29336ff8b5dc157a76be7e0889e8
Prakash Adhikari <pa7846665@gmail.com>
2024-11-15 23:14:16 +0530

    frontend build done

--- client/src/utils/ApiURI.jsx
+++ client/src/utils/ApiURI.jsx
@@ -1,2 +1,5 @@
-export const API = "http://localhost:8000/api/v1"
-export const PRODUCT_FILES = "http://localhost:8000/api"
+// export const API = "http://localhost:8000/api/v1"
+// export const PRODUCT_FILES = "http://localhost:8000/api"
+export const API = "https://assignment-lzpq.onrender.com/api/v1"
+
+export const PRODUCT_FILES = "https://assignment-lzpq.onrender.com/api"
